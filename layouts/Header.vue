bf1bd8e7a524ac98cbe5a766283f8279850622c6
xiaoluoboding <xiaoluoboding@gmail.com>
2020-06-07 23:08:27 +0800

    fix: v-if build error

--- layouts/Header.vue
+++ layouts/Header.vue
@@ -80,7 +80,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
               </svg>
               <!-- Menu open: "hidden", Menu closed: "block" -->
-              <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24" v-else>
+              <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24" v-if="!isShowMobileMenu">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
             </button>
